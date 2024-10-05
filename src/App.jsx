9743805f9ebe2a9256d21a9b9d614b0419961426
Hesham <Hesham Abdelnasser@DESKTOP-D15PJJA>
2024-10-06 02:38:21 +0300

    sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./screens/Home";
+// import Home from "./screens/Home";
+import SignUp from "./components/signUp/signUp";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <SignUp />
     </>
   );
 }
