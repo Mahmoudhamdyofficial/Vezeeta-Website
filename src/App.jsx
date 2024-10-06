8080c60ad2c84b61cf4473d3b65d4c5827967807
Hesham <Hesham Abdelnasser@DESKTOP-D15PJJA>
2024-10-06 04:33:32 +0300

    search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+  
 // import Home from "./screens/Home";
-import SignUp from "./components/signUp/signUp";
+// import SignUp from "./components/signUp/signUp";
+import Search from "./components/search/search";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Search />
     </>
   );
 }
