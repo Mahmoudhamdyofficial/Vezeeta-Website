f1ff77e9268568686733e553f965a1ad38d492f0
mahmoudhamdy <mahmoud.hamdy9993@gmail.com>
2024-10-08 18:33:44 +0300

    Teleconsultation

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from "./screens/Home";
 import Login from "./screens/login";
 import About from "./screens/About";
+import Teleconsultation from "./screens/allspecialities/Teleconsultation";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <Login /> */}
-      <About />
-
+      {/* <About /> */}
+      <Teleconsultation />
 
     </>
   );
