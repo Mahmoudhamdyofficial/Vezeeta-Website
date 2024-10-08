7ec38d92eaa124f1edf602b5bcfb7ff1ff4aa1d6
mohamedMahmoudElkady <mohamedmahmoudelkady111@gmail.com>
2024-10-08 19:40:22 +0300

    about,login and teleconsultation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from "./screens/Home";
+import Login from "./screens/login";
+import About from "./screens/About";
+import Teleconsultation from "./screens/allspecialities/Teleconsultation";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      {/* <Login /> */}
+      {/* <About /> */}
+      <Teleconsultation />
+
     </>
   );
 }
